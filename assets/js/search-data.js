18a9d96fe0cd409f58cb69880d02a3dc0b3a4a2a
Joey Dumont <joey.dumont@gmail.com>
2024-03-18 21:45:45 -0400

    Hopefully nothing breaks, this is a huge Jekyll update.

---
+++ assets/js/search-data.js
@@ -0,0 +1,171 @@
+var store = [{
+        "title": "Sent from iPod touch.",
+        "excerpt":"This post was published to Blogger using my iPod touch. That&#39;s really cool!","categories": [],
+        "tags": [],
+        "url": "https://blog.joey-dumont.ca/sent-from-ipod-touch/"
+      },{
+        "title": "Project",
+        "excerpt":"I am in want of a project. Groupe de travail won&#39;t take all my timethis summer although the house could. Maybe compiling notes onmathematics and physics or something like that.I&#39;ll meditate on that and talk to you later.","categories": [],
+        "tags": [],
+        "url": "https://blog.joey-dumont.ca/project/"
+      },{
+        "title": "Finally, a new home",
+        "excerpt":"&nbsp;Good news! The blog has a now a new URL: blog.joey-dumont.ca. I have consolidated my other projects (such as Groupe de Travail, for physics students) and my writing projects to my main site.I will shortly have a portal that summarizes all my projects in one page. It will live on...","categories": [],
+        "tags": [],
+        "url": "https://blog.joey-dumont.ca/finally-new-home/"
+      },{
+        "title": "A Little History",
+        "excerpt":"&nbsp;&nbsp;&nbsp;I've been doing a little research on the origins of Q.E.D., the latin expression that is usually used to conclude mathematical proofs. Turns out it was Euclid that started it all about 2300 years ago. The original Greek expression is ὅπερ ἔδει δεῖξαι and means \"precisely what was to be...","categories": [],
+        "tags": ["qed","history","mathematics"],
+        "url": "https://blog.joey-dumont.ca/little-history/"
+      },{
+        "title": "GCal push notifications on your iPod",
+        "excerpt":"&nbsp;Recently, Google updated its Google Mobile app for the iPhone/iPod touch. Lifehacker almost instantly pushed out the information to the geeks. However, they failed to mention one small detail.I tried this when it went out the first time. For some reason, I received notifications in the form of badges for...","categories": [],
+        "tags": ["google calendar","mobile","colour sync","push notifications","iphone"],
+        "url": "https://blog.joey-dumont.ca/gcal-push-notifications-on-your-ipod/"
+      },{
+        "title": "Perhaps there is a stale lock file?",
+        "excerpt":"&nbsp;&nbsp;I recently ran into a seemingly severe problem with my computer. Since approximately 2003, I've been using Linux as my primary desktop. I have enjoyed the learning that inevitably came out of it, and have also joined the great community of Linux users.&nbsp;&nbsp; Anyhow, when I booted my computer yesterday,...","categories": [],
+        "tags": ["sleep","transmission","linux","bittorent","mtab","stale lock file"],
+        "url": "https://blog.joey-dumont.ca/perhaps-there-is-stale-lock-file/"
+      },{
+        "title": "Equations in Blogger",
+        "excerpt":"&nbsp;&nbsp; Sweet! I can use equations in Blogger! I mean, LaTeX equations. This site has designed a script that allows LaTeX code to be used in Blogger. So I can present Gauss' Law for gravity in a very clear manner, as I now have the tool to do it. So,...","categories": [],
+        "tags": [],
+        "url": "https://blog.joey-dumont.ca/equations-in-blogger/"
+      },{
+        "title": "iPod touch Gets a Taste of What It Is to be an iPad",
+        "excerpt":"&nbsp;[iOS 4.x]&nbsp;If you’re the owner of a jailbroken iPod touch and wanted your screen to rotate just like the iPad can, SBRotator 4.x does just that. The application, available through Cydia, has launched today. SBRotator in Cydia App StoreIt rotates your SpringBoard in any orientation you want. The icons are...","categories": [],
+        "tags": ["rotation","orientation","springboard","jailbreak","cydia","iphone","ipod touch"],
+        "url": "https://blog.joey-dumont.ca/ipod-touch-gets-taste-of-what-it-is-to/"
+      },{
+        "title": "Integrability and Chaos in Hamiltonian Systems",
+        "excerpt":"&nbsp; &nbsp;This last semester, I took a course intitled Nonlinear Dynamics and Chaos&nbsp;(in &nbsp;French). It was quite a novelty, as undergraduates in physics mostly deal with linear problems (they are much easier to solve analytically. Most nonlinear systems do not possess such solutions and they do, the&nbsp;analytic&nbsp;expression is so complex...","categories": [],
+        "tags": ["beamer","hamiltonian mechanics","latex","mechanics","hamilton","dynamics","physics","nonlinear dynamics"],
+        "url": "https://blog.joey-dumont.ca/integrability-and-chaos-in-hamiltonian/"
+      },{
+        "title": "IBM turns 100!",
+        "excerpt":"&nbsp; &nbsp; IBM has just turned 100. To celebrate this stellar achievement, the company has produced a video that summarizes its century of existence in a rather poignant way.&nbsp;Enjoy![Video Link]&nbsp;IBM Centennial Film&nbsp;&nbsp;&nbsp;","categories": [],
+        "tags": ["tech","technology","IBM","birthday"],
+        "url": "https://blog.joey-dumont.ca/ibm-has-just-turned-100/"
+      },{
+        "title": "Programming is fun!",
+        "excerpt":"A look at my coding environment&nbsp; &nbsp;I've started programming about a month ago. I pretty much learned by myself by reading a book or two about C++. Oh, and I skimmed some parts of C++ GUI Programming with Qt 4, by Jasmin Blanchette and Mark Summerfield. So far, I'm liking...","categories": [],
+        "tags": ["c++","qt4","programming","gui"],
+        "url": "https://blog.joey-dumont.ca/programming-is-fun/"
+      },{
+        "title": "Removing files from working directory not under version control",
+        "excerpt":"&nbsp; &nbsp;I've been using SVN since the beginning of summer and I'm liking it so far. However, I stumbled across a little problem this week.&nbsp; &nbsp; I use SVN to write a LaTeX document which spans multiple files. Being textual, the contents of the .tex files are diff-able and all...","categories": [],
+        "tags": ["command-line","programming","linux","bash","svn"],
+        "url": "https://blog.joey-dumont.ca/removing-files-from-working-directory/"
+      },{
+        "title": "Return user-defined sets in PostgreSQL",
+        "excerpt":"&nbsp; &nbsp;Returning sets of data with PL/pgSQL functions can be somewhat tricky. The documentation has a line saying how to do it somewhere, but the beginner SQL user (like the author of this post) might be confused at the impressive amount of information given by this document.&nbsp;&nbsp; This post is...","categories": [],
+        "tags": ["database","programming","linux","postgresql"],
+        "url": "https://blog.joey-dumont.ca/return-user-defined-sets-in-postgresql/"
+      },{
+        "title": "How-to make LaTeX footnotes stick to the bottom of the page",
+        "excerpt":"&nbsp; &nbsp;Disclaimer. The credit goes all to&nbsp;Nikos Skalkotos, on whose blog I found this. I couldn't help but repost it here.&nbsp; &nbsp; Sometimes, when you write something in LaTeX which has a number of figures, some pages have a lot of whitespace. If you also have a footnote on this...","categories": [],
+        "tags": ["tech","latex"],
+        "url": "https://blog.joey-dumont.ca/how-to-make-latex-footnotes-stick-to/"
+      },{
+        "title": "Contribution to the Book of Knowledge",
+        "excerpt":"The old&nbsp;version of Wikipedia. Yes, this is a joke.&nbsp;&nbsp; &nbsp; &nbsp;I just made my first contribution to Wikipedia. Considering that it is the first place when I need to check something quickly, or to get references for specific subjects since it launched (yes, I was 11 years old at the...","categories": [],
+        "tags": ["donation","science","wikipedia"],
+        "url": "https://blog.joey-dumont.ca/contribution-to-book-of-knowledge/"
+      },{
+        "title": "Corrrupted SVN repo and the solution",
+        "excerpt":"&nbsp; &nbsp;I'm currently taking a Numerical Physics course at U. Laval. So far, it has been a really enjoyable experience. However, I would like to share some technical know-how I acquired while working on one of the projects.&nbsp; &nbsp;We are mostly using MATLAB (I personally use Octave, which is the...","categories": [],
+        "tags": ["command-line","sourceforge","svn"],
+        "url": "https://blog.joey-dumont.ca/currently-taking-numerical-physics/"
+      },{
+        "title": "Equations in Blogger (part 2)",
+        "excerpt":"&nbsp;&nbsp;A while ago, I fumbled into a method to typeset equations on any web page. However, the JavaScript needed to do the work suddently disappeared (I didn't research that) and anyhow, the equations stopped displaying properly.&nbsp;Now I just found out that MathJax&nbsp;is available as CDN service. This makes my life...","categories": [],
+        "tags": ["meta","latex","blogger","physics"],
+        "url": "https://blog.joey-dumont.ca/equations-in-blogger-part-2/"
+      },{
+        "title": "Printer margins issues with Samsung CLP-310N",
+        "excerpt":"&nbsp; &nbsp;Although this is a rather specific issue concerning the Samsung CLP-310N, I thought it could be a good idea to share the solution to a longstanding problem I was having.&nbsp;&nbsp; &nbsp;For some reason, when I upgraded CUPS some time ago (I don't recall specifically when this began), printing PDFs...","categories": [],
+        "tags": ["drivers","cups","linux","samsung"],
+        "url": "https://blog.joey-dumont.ca/printer-margins-issues-with-samsung-clp/"
+      },{
+        "title": "Lattice Gauge Theory",
+        "excerpt":"&nbsp; &nbsp;During the Winter 2012 semester at U. Laval, I took a Particle Physics course. I'm not sure of the exact appelation in English, but it was given as directed reading (lectures dirigées). I didn't have classes: only the reference book.&nbsp; &nbsp;None of that really matters anyway. The course culminated...","categories": [],
+        "tags": ["physics"],
+        "url": "https://blog.joey-dumont.ca/lattice-gauge-theory/"
+      },{
+        "title": "Issues with Linux and HP printers",
+        "excerpt":"&nbsp; &nbsp;Since I have access to a new printer, I (of course) had issues with it. As I tried different kinds of connection with CUPS (ipp://, ipps://, http://, socket://) and different drivers (hpijs, hpcups), the page would sometimes render blank lines as black strips of ink or print with some...","categories": [],
+        "tags": ["hp","cups","linux","printer"],
+        "url": "https://blog.joey-dumont.ca/issues-with-linux-and-hp-printers/"
+      },{
+        "title": "Fortran, C++ and qmake",
+        "excerpt":"Qt Creator Logo&nbsp; &nbsp;Call me a noob, but I love using Qt Creator even when I'm not programming a GUI. The UI greatly facilitates code writing: the syntax highlighting is very customizable, it shows you the methods of a class when you use the . or -&gt; operators, it makes...","categories": [],
+        "tags": ["c++","programming","fortran","linux","numerical_physics","physics"],
+        "url": "https://blog.joey-dumont.ca/fortran-c-and-qmake/"
+      },{
+        "title": "Bessel functions of complex argument",
+        "excerpt":"&nbsp; &nbsp;Our research group, Dynamica, has openly published a shared object C++ library to compute Bessel functions of complex argument. It is available ready to compile as a tar archive or, for the initiated, as C++ and Fortran source code on Google Code.&nbsp; &nbsp;The Fortran code comes from D.E. Amos&nbsp;and...","categories": [],
+        "tags": ["c++","programming","fortran","numerical_physics","physics"],
+        "url": "https://blog.joey-dumont.ca/bessel-functions-of-complex-argument/"
+      },{
+        "title": "(Literary) Ramblings about the Universe",
+        "excerpt":"&nbsp; &nbsp;While sipping away at my alcoholic drink, I had, as men usually have, philosophical matters spring to my mind. Wrought body and soul in the shadow of the night, my first instinct was to share my thoughts with the world.&nbsp; &nbsp;In Siddhartha, by Hermann Hesse, a young prodigy of...","categories": [],
+        "tags": ["still_point","philosophy"],
+        "url": "https://blog.joey-dumont.ca/literary-ramblings-about-universe/"
+      },{
+        "title": "Arrow in the Knee: the Genesis",
+        "excerpt":"&nbsp;&nbsp;&nbsp; So my wife was re-reading the epic fantasy pentalogy The Belgariad by David Eddings [1]. It's a fantastic coming-of-age, prophecy-fulfilling, magic-filled story. Every character is fully developed and they are all thoroughly likable. &nbsp;&nbsp;&nbsp; But now to the important part. In the Queen of Sorcery book [2,p.222], originally published...","categories": [],
+        "tags": [],
+        "url": "https://blog.joey-dumont.ca/arrow-in-knee-genesis/"
+      },{
+        "title": "Interfacing Armadillo and FFTW",
+        "excerpt":"&nbsp;&nbsp;&nbsp;FFTW, or the Fastest Fourier Transform in the West, is an awesome C library used to, well, perform FFTs. Armadillo, on the other hand, is an awesome C++ library that implements linear algebra operations. Both are renowned for their amazing speed and, especially the latter, ease of use. &nbsp;&nbsp;&nbsp;Now suppose...","categories": [],
+        "tags": ["c++","programming","fftw","armadillo"],
+        "url": "https://blog.joey-dumont.ca/interfacing-armadillo-and-fftw/"
+      },{
+        "title": "Dual basis and its applications",
+        "excerpt":"&nbsp;&nbsp;&nbsp;In the physical sciences, the scientist mostly tries to put the problem in a form that is easily solvable. One such form is the eigenvector expansion where one solves a simpler problem and assumes that the more difficult problem can be decomposed in a sum of the simpler solutions. &nbsp;&nbsp;&nbsp;This...","categories": [],
+        "tags": [],
+        "url": "https://blog.joey-dumont.ca/dual-basis-and-its-applications/"
+      },{
+        "title": "Google Calendar Material Design",
+        "excerpt":"&nbsp;&nbsp;&nbsp;My Nexus 4 prompted me last night for permission to update Gmail and Google Calendar. I must say, while Gmail looks slightly better than it did, GCal has received a much needed revamp, especially the widget. I do not have a screenshot of the previous version, but the colour scheme...","categories": [],
+        "tags": ["google calendar","nexus"],
+        "url": "https://blog.joey-dumont.ca/google-calendar-material-design/"
+      },{
+        "title": "Have yaourt save compiled packages to /var/cache/pacman/pkg",
+        "excerpt":"&nbsp;&nbsp;&nbsp;The title of this post might not mean much to most of you, but for those few lucky enough to use Arch Linux on a daily basis, this could be of use to you. By default, yaourt does not keep the package files it produces, it deletes them after installation....","categories": [],
+        "tags": ["command-line","yaourt","linux","arch-linux"],
+        "url": "https://blog.joey-dumont.ca/have-yaourt-save-compiled-packages-to/"
+      },{
+        "title": "vnc through SSH: how to control your desktop remotely",
+        "excerpt":"I won’t go into too much detail in this post, as there are multiple guidesthat go above and beyond in explaining the behind-the-scenes of tunnellingVNC connections through SSH. Note that this solution uses TigerVNC’s x0vncserverto control the remote X server. Simply use vncserver if you want to controla different X...","categories": [],
+        "tags": ["vnc","ssh","remote desktop","linux"],
+        "url": "https://blog.joey-dumont.ca/vnc-through-ssh-how-to-control-your-desktop-remotely/"
+      },{
+        "title": "Loading complex numbers in the C++ format from text files into NumPy",
+        "excerpt":"In my workflow, I typically use C++ for production code and Python for datapost-processing and data analysis. A major annoyance is that NumPy’sgenfromtxt does not recognize the C++ complex number format. Of course, one could write their own I/O for their C++ production codes, butmost libraries have built-in I/O functions...","categories": [],
+        "tags": ["complex numbers","numpy","C++","genfromtxt"],
+        "url": "https://blog.joey-dumont.ca/read-c-format-complex-numbers-with-numpy/"
+      },{
+        "title": "Converting between different Zernike polynomial index systems",
+        "excerpt":"The Zernike polynomials are a very popular basis to describe aberrations in thewavefront of optical beams. They are used in ophthalmology  [1], microscopy, and laser metrology, among others  [2]. Because they are orthogonal on the unit disk, they are mostsuitable to describe deviations from a flat phase front in a...","categories": [],
+        "tags": ["Zernike polynomials","indexing conventions","Noll","Phasics"],
+        "url": "https://blog.joey-dumont.ca/zernike-polynomials-coefficients/"
+      },{
+        "title": "Building a multilib MIPS toolchain: my journey through gcc's build system",
+        "excerpt":"This article assumes that you know to compile a cross-compiler from scratch. Ifnot, I recommend this succinct guide, or this longer guide if you wantto have some context. On x86_64 architectures, building gcc such that it can generate both 32- and64-bit code is easy: just use the --enable-multilib flag at...","categories": [],
+        "tags": ["gcc","multilib","multi ABI","mips64-elf","mips64","mips","toolchains"],
+        "url": "https://blog.joey-dumont.ca/building-a-multilib-mips-toolchain/"
+      },{
+        "title": "2021 in Music",
+        "excerpt":"I’ve been meaning to write a brief retrospective of the all the albums I’ve listened to in the past year for a couple years now, but I usually either forget to take note of albums I really enjoyed, or lost track of all the releases. This year’s listicle is brought...","categories": [],
+        "tags": ["year review","music","prog rock","prog metal"],
+        "url": "https://blog.joey-dumont.ca/2021-in-music/"
+      },{
+        "title": "Numen Review",
+        "excerpt":"I mentioned in my last post1 that I was eagerly waiting for the next Alkaloid album, and that To Where the Light Retreats was a good way to pass the time, but I am no longer waiting. Numen came out in September 2023, and while it is not everything I...","categories": ["music"],
+        "tags": ["music","alkaloid","numen","review","prog metal"],
+        "url": "https://blog.joey-dumont.ca/music/numen-review/"
+      }]
